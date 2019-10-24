a9b9f3d712511a978336515419ec2613000c5732
Xavi Ruiz <xaru8145@colorado.edu>
2019-10-24 16:14:04 +0000

    got the show_center_depth.py

--- realsense-ros/realsense2_camera/scripts/show_center_depth.py
+++ realsense-ros/realsense2_camera/scripts/show_center_depth.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import rospy
 from sensor_msgs.msg import Image as msg_Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -14,7 +16,7 @@ class ImageListener:
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, data.encoding)
             pix = (data.width/2, data.height/2)
-            sys.stdout.write('%s: Depth at center(%d, %d): %f(mm)\r' % (self.topic, pix[0], pix[1], cv_image[pix[1], pix[0]]))
+            sys.stdout.write('%s: Depth at center(%d, %d): %f(mm)\n' % (self.topic, pix[0], pix[1], cv_image[pix[1], pix[0]]))
             sys.stdout.flush()
         except CvBridgeError as e:
             print(e)
