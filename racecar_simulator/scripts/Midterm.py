3a06367da9858b2b2904b74f6395db24dda4cbef
Thomas <thlu1684@colorado.edu>
2019-10-27 14:20:01 -0600

    Midterm Inital Run

--- racecar_simulator/scripts/Midterm.py
+++ racecar_simulator/scripts/Midterm.py
@@ -11,15 +11,17 @@ drive_commands = MotorCommand()
     #float64 position
     #float32 speed
     #float32 acceleration
+    # DC Motor Min 0.1 Max 0.45 (0.62 but we don't see a change)
 servo_commands.joint_name = 'servo' #Check name here
 drive_commands.joint_name = 'drive' #Check name here
-drive_commands.position = 0
+drive_commands.position = 0.1 #Set speed here
 drive_commands.speed = 0
 drive_commands.acceleration = 0
 servo_commands.position = 0
 servo_commands.speed = 0
 servo_commands.acceleration = 0
-
+#State Machine Variable
+car_state = 1
 
 pub = rospy.Publisher('/pololu/command', MotorCommand, queue_size=10)
 #pub = rospy.Publisher('/drive', AckermannDriveStamped, queue_size=10)
@@ -28,6 +30,7 @@ pub = rospy.Publisher('/pololu/command', MotorCommand, queue_size=10)
 
 def callback(data):
     global servo_commands
+    global car_state
     lx = len(data.data)
     x_red = []
     N = 5
@@ -36,16 +39,62 @@ def callback(data):
 	x_red.append(np.average([x for x in bin_vals if x >= 10]))
     #x_red[0] is far left and x_red[end] is far right
     #Min value is 100 for reading
-    #Servo -0.54 to + 0.54
-    th = 800
-    if x_red[0] < th and x_red[2] > th and x_red[4] > th:
-        servo_commands.position = 0.5
-    elif x_red[0] > th and x_red[2] > th and x_red[4] > th:
+    #Servo -0.54(left) to + 0.54(right)
+    #Threshold values for turning
+    th1 = 1000
+    th2 = 800
+    th3 = 700
+    th4 = 500
+    if car_state == 1: #Driving straight in the hallway
+        #Turns car to the right
+        if x_red[0] < th1 and x_red[2] > th1 and x_red[-1] > th1:
+            print("Turing Right")
+            if x_red[0] < th2:
+                servo_commands.position = 0.2
+            elif x_red[0] < th3:
+                servo_commands.position = 0.3
+            elif x_red[0] < th4:
+                servo_commands.position = 0.4
+            else:
+                servo_commands.position = 0.1
+        #Turns car to the Left
+        elif x_red[0] > th1 and x_red[2] > th1 and x_red[-1] < th1:
+            print("Turing Left")
+            if x_red[-1] < th2:
+                servo_commands.position = -0.2
+            elif x_red[-1] < th3:
+                servo_commands.position = -0.3
+            elif x_red[-1] < th4:
+                servo_commands.position = -0.4
+            else:
+                servo_commands.position = -0.1
+        #Turns car to the Straight
+        elif x_red[0] > th1 and x_red[2] > th1 and x_red[-1] > th1:
+            print("Driving Straight")
+            servo_commands.position = 0
+        #State transitions
+        if x_red[2] < 2000 and x_red[0] < 2000 and x_red[-1] > 2000:
+            car_state = 2 # Going around the corner
+        if x_red[0] < 500 and x_red[1] < 500 and x_red[2] < 500 and x_red[3] < 500 and x_red[4] < 500:
+            car_state = 0 # Stopping the car
+    #State for turing the corner
+    elif car_state = 2:
+        print("Turing this Bitch around dat corner")
+        servo_commands.position = 0.1
+        if x_red[0] < 2000 and x_red[2] > 5000 and x_red[-1] < 2000:
+            car_state = 1
+            print("We Made it?")
+        if x_red[0] < 500 and x_red[1] < 500 and x_red[2] < 500 and x_red[3] < 500 and x_red[4] < 500:
+            car_state = 0 # Stopping the car
+    #State for stopping the car
+    elif car_state = 0:
+        print("Either we Won or We Fucked up")
+        drive_commands.position = 0 #Set speed here
+        drive_commands.speed = 0
+        drive_commands.acceleration = 0
         servo_commands.position = 0
-    elif x_red[0] > th and x_red[2] > th and x_red[4] < th:
-        servo_commands.position = -0.5
-    else:
-        servo_commands.position = 0
+        servo_commands.speed = 0
+        servo_commands.acceleration = 0
     pub.publish(servo_commands)    
     print("ranges left to right", x_red)
 
