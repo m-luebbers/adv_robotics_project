4d3e171666f375f8ac64244ec72254864d0186b5
Xavi Ruiz <xaru8145@colorado.edu>
2019-10-27 18:55:43 +0000

    binned depth data

--- racecar_simulator/scripts/Midterm.py
+++ racecar_simulator/scripts/Midterm.py
@@ -2,7 +2,7 @@
 import rospy
 from std_msgs.msg import Float32MultiArray
 # from ackermann_msgs.msg import AckermannDriveStamped
-from ros_pololu_servo import MotorCommand
+from ros_pololu_servo.msg import MotorCommand
 # from sensor_msgs.msg import LaserScan
 import numpy as np
 servo_commands = MotorCommand()
@@ -15,19 +15,24 @@ servo_commands.joint_name = 'servo' #Check name here
 drive_commands.joint_name = 'drive' #Check name here
 drive_commands.position = 0
 drive_commands.speed = 0
-drive_commands.acceleration = 
+drive_commands.acceleration = 0
 servo_commands.position = 0
 
 
 
-pub = rospy.Publisher('/drive', AckermannDriveStamped, queue_size=10)
+#pub = rospy.Publisher('/drive', AckermannDriveStamped, queue_size=10)
 
     # rate.sleep()    
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data)
-     x = data
-     print(x)
+    lx = len(data.data)
+    x_red = []
+    N = 10
+    for ix in range(N):
+	bin_vals = data.data[int(round(lx*ix/N)):int(round(lx*(ix+1)/N))]
+	x_red.append(np.average([x for x in bin_vals if x >= 10]))
+    
+    print("ranges left to right", x_red)
 
 
 def depth_data_processor():
